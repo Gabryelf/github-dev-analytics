dd1c194885dc2971fc504e1317b507f94076fcae
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2025-11-08 22:04:17 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};

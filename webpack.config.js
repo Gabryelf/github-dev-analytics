5b5d3d16d31b7f0ce58a58e6da724ab575c897fc
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2025-11-08 22:15:34 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,38 +6,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      inject: true,
     }),
   ],
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
